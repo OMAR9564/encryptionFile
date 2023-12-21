978bf6fd442522ba4602184e8ebbac70d383a572
Ömer Faruk <teknopluse12@gmail.com>
2023-12-21 21:54:50 +0300

    modify encrypt and add decrypt functions

--- EncryptDecryptApplication/src/EncryptDecryptApplication.java
+++ EncryptDecryptApplication/src/EncryptDecryptApplication.java
@@ -1,5 +1,7 @@
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -11,9 +13,14 @@ import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
 
 public class EncryptDecryptApplication {
     private static final String ALGORITHM = "AES";
+
+    private static final String encryptExtansion = "ENCRYPT.bya";
+
     private static final int KEY_LENGTH = 256;
     private JPasswordField passwordField;
     private JLabel lblPass;
@@ -44,24 +51,24 @@ public class EncryptDecryptApplication {
         encryptDecryptButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try{
-
+                try {
                     char[] passwordChar = passwordField.getPassword();
-                    //remove spaces for most security
-
-                    if((passwordChar.length > 0)){
-                        Key key = generateKey();
-                        System.out.println(key);
+                    if (passwordChar.length > 0) {
+                        Key key = generateKey(Arrays.toString(passwordChar));
                         String password = new String(passwordChar);
                         password.replace(" ", "");
-                        encrypt(password, new File(openFileTextField.getText()));
 
-                    }
-                    else{
+                        String filedText = openFileTextField.getText();
+                        if (filedText.contains(encryptExtansion)){
+                            decrypt(key, new File(openFileTextField.getText()));
+                        }else {
+                            encrypt(key, new File(openFileTextField.getText()));
+                        }
+                    } else {
                         JOptionPane.showMessageDialog(null, "Please enter the password",
                                 "Error", JOptionPane.ERROR_MESSAGE);
                     }
-                }catch (Exception exception){
+                } catch (Exception exception) {
                     exception.printStackTrace();
                 }
             }
@@ -81,41 +88,51 @@ public class EncryptDecryptApplication {
             }
         });
     }
-    private static Key generateKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
-        // Generate a random key of the specified length
-        byte[] keyBytes = new byte[KEY_LENGTH / 8];
-        SecureRandom secureRandom = new SecureRandom();
-        secureRandom.nextBytes(keyBytes);
 
-        // Create a SecretKeySpec object from the key bytes
+
+    private static Key generateKey(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        // 16byte -> 128bit
+        KeySpec keySpec = new PBEKeySpec(password.toCharArray(), new byte[16], 65536, KEY_LENGTH);
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+        byte[] keyBytes = secretKeyFactory.generateSecret(keySpec).getEncoded();
+
+        // Create a SecretKeySpec object from the derived key bytes
         return new SecretKeySpec(keyBytes, ALGORITHM);
     }
 
-    public static void encrypt(String passwordKey, File inputFile) throws Exception {
-        //Create encrypt password
-        byte[] passwordBytes = passwordKey.getBytes();
+
+    public static void encrypt(Key passwordKey, File inputFile) throws Exception {
+        performCrypto(Cipher.ENCRYPT_MODE, passwordKey, inputFile);
+    }
+
+    public static void decrypt(Key passwordKey, File inputFile) throws Exception {
+        performCrypto(Cipher.DECRYPT_MODE, passwordKey, inputFile);
+    }
+
+    private static void performCrypto(int cipherMode, Key passwordKey, File inputFile) throws Exception {
+        byte[] passwordBytes = passwordKey.getEncoded();
         SecretKey key = new SecretKeySpec(passwordBytes, "AES");
 
         Cipher cipher = Cipher.getInstance("AES");
-        //Select encrypt method
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-
+        cipher.init(cipherMode, key);
 
         FileInputStream inputStream = new FileInputStream(inputFile);
         String fileName = inputFile.getName();
 
-        //convert file to bytes
         byte[] inputBytes = new byte[inputStream.available()];
         inputStream.read(inputBytes);
         byte[] outputBytes = cipher.doFinal(inputBytes);
 
-        //save encrypt file
-        FileOutputStream outputStream = new FileOutputStream(fileName + "ENCRYPT.bya");
+        String outputFileName = cipherMode == Cipher.ENCRYPT_MODE ?
+                fileName + encryptExtansion : fileName.replace(encryptExtansion, "");
+
+        FileOutputStream outputStream = new FileOutputStream(outputFileName);
         outputStream.write(outputBytes);
 
         inputStream.close();
         outputStream.close();
     }
+
 
 
 }
